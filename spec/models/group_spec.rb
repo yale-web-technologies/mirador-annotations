1edbec838d7d33aff0091e7eb9ca0b7fb5fc201c
rlechich <roy.lechich@yyale.edu>
2015-09-30 10:24:51 -0400

    users<==>groups has_and_belongs_to_many association

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,18 +1,73 @@
 require 'rails_helper'
-#require 'shoulda/matchers'
+
+include Warden::Test::Helpers
+Warden.test_mode!
+include Devise::TestHelpers
+
 
 RSpec.describe Group, type: :model do
 
   before (:each) do
-    @grp ='{"group_id": "http://localhost:5000/groups/testGroup", "description":"test group"}'
+    @usr ='{"uid":"jasper99", "password":"pass-word", "email":"jasper99@yale.edu", "encrypted_password":"7KVcbLRkKU15XiCRlTGuj0raudw+pl+SaGVnm456LoE", "provider":"cas", "sign_in_count":"0"}'
+    @grp='{"group_id": "http://localhost:5000/groups/testGroup", "group_description":"test group"}'
+    @acl1 ='{"resource_id":"http://localhost:5000/layers/testLayer1", "acl_mode": "read", "group_id": "http://localhost:5000/groups/testGroup"}'
+    @acl2 ='{"resource_id":"http://localhost:5000/layers/testLayer2", "acl_mode": "write", "group_id": "http://localhost:5000/groups/testGroup"}'
+    @acl3 ='{"resource_id":"http://localhost:5000/layers/testLayer3", "acl_mode": "read", "group_id": "http://localhost:5000/groups/testGroup3"}'
+    #@user= User.create!(JSON.parse(@usr))
+    @user = FactoryGirl.create(:jasper99)
+
     @group= Group.create(JSON.parse(@grp))
+    @grpwebacl1 = '{"group_id":"http://localhost:5000/groups/testGroup", "resource_id":"http://localhost:5000/layers/testList1"}'
+    @grpwebacl2 = '{"group_id":"http://localhost:5000/groups/testGroup3", "resource_id":"http://localhost:5000/layers/testList2"}'
+    #@group.webacls.create JSON.parse( @grpwebacl1)
+    #@group.webacls.create JSON.parse( @grpwebacl2)
+    @usrgrp = '{"user_id":"jasper99","group_id":"http://localhost:5000/groups/testGroup"}'
+    @usrgrp2 = '{"user_id":"jasper77","group_id":"http://localhost:5000/groups/testGroup"}'
+    @group.users.create JSON.parse(@usrgrp)
+    @group.users.create JSON.parse(@usrgrp2)
+
+    @webAcl1= Webacl.create(JSON.parse(@acl1))
+    @webAcl2= Webacl.create(JSON.parse(@acl2))
+    @webAcl3= Webacl.create(JSON.parse(@acl3))
   end
 
-  describe Group do
-   it { is_expected.to have_many :webacls}
+  context 'Record Associations' do
+    describe 'Group associations' do
+      it "returns a hasMany association with Group" do
+        expect(Group.reflect_on_association(:users).class.to_s).to eq("ActiveRecord::Reflection::HasAndBelongsToManyReflection")
+      end
+      it "returns a hasThrough association with Webacl" do
+        #expect(Group.reflect_on_association(:webacls).class.to_s).to eq("ActiveRecord::Reflection::HasAndBelongsToManyReflection")
+        expect(Group.reflect_on_association(:webacls).class.to_s).to eq("ActiveRecord::Reflection::HasManyReflection")
+      end
+    end
   end
 
-  describe Group do
-    #it { should belong_to :user }
+  context 'Groups Associated resources' do
+    describe 'getGroupsResources' do
+      it "returns all a group's webacls, based on the group" do
+        resourceIds = Group.getGroupsResourceIds @group
+        expect(resourceIds.length).to eq(2)
+      end
+    end
   end
+
+  context 'Groups Associated users' do
+    describe 'getGroupsUsers' do
+      it "returns all a group's users, based on the group" do
+        userIds = Group.getGroupsUserIds @group
+        expect(userIds.length).to eq(2)
+      end
+    end
+  end
+
+  after(:each) do
+    @user.destroy
+    @group.destroy
+    @webAcl1.destroy
+    @webAcl2.destroy
+    @webAcl3.destroy
+  end
+
+
 end
