270d86ceecb4bcc9fc0aa390176ae5476dfb184a
rlechich <roy.lechich@yyale.edu>
2016-02-10 13:28:28 -0500

    change auto-id for users_webacls create migration

--- db/migrate/20150929010929_create_users_webacls.rb
+++ db/migrate/20150929010929_create_users_webacls.rb
@@ -1,6 +1,7 @@
 class CreateUsersWebacls < ActiveRecord::Migration
   def change
-    create_table :users_webacls, id: :false do |t|
+    #create_table :users_webacls, id: :false do |t|
+    create_table :users_webacls do |t|
       t.belongs_to :user, index: :true
       t.belongs_to :group, index: :true
       t.belongs_to :webacl, index: :true
