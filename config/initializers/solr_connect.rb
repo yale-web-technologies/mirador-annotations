075e29f07f5e3af3c983d97c9c3a22125ef46b44
rlechich <roy.lechich@yyale.edu>
2016-09-20 12:10:14 -0400

    adds solr feed for consumption via REST API (...)

--- config/initializers/solr_connect.rb
+++ config/initializers/solr_connect.rb
@@ -1,10 +1,10 @@
-SolrConnectConfig.setup do |config|
+#SolrConnectConfig.setup do |config|
 
-  miradorSolrUrl = ENV['LOTB_SOLR_URL']
-  config.set("solrUrl", miradorSolrUrl)
+#  miradorSolrUrl = ENV['LOTB_SOLR_URL']
+#  config.set("solrUrl", miradorSolrUrl)
 
-  p "solr.connect.rb: miradorSolrUrl = #{miradorSolrUrl}"
+#  p "solr.connect.rb: miradorSolrUrl = #{miradorSolrUrl}"
 
-  exported_manifests_path = ENV['lotb_MANIFESTS_PATH']
+#  exported_manifests_path = ENV['lotb_MANIFESTS_PATH']
 
-end
+#end
