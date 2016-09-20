0ea6f8a549cd682266e7a86032805453f41f4e13
rlechich <roy.lechich@yyale.edu>
2016-09-20 12:23:43 -0400

    increases timeout param in config/unicorn.rb to 60 seconds

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,8 @@
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 #timeout 15
 # was timing out for LotB prod
-timeout 30
+#timeout 30
+timeout 60
 preload_app true
 
 before_fork do |server, worker|
