289813a66de79b0ffce79dbee0bdb9cdcf88912f
rlechich <roy.lechich@yyale.edu>
2017-04-05 10:07:27 -0400

    ...

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -302,8 +302,8 @@ class Annotation < ActiveRecord::Base
 
       begin
         panel = anno.annotation_id[panelIndex..chapterIndex-2].gsub!(/_/," ")
-        if (panel=="Panel A" panel = "Panel 1")
-        if (panel=="Panel B" panel = "Panel 2")
+        panel = "Panel 1" if panel=="Panel A"
+        panel = "Panel 2" if panel=="Panel B"
         chapter = anno.annotation_id[chapterIndex..sceneIndex-2].gsub!(/_/," ")
         scene = "Scene " + sceneNumber if !sceneNumber.nil?
         # end LOTB panels, chapters and scenes
