e63e35c3b48c1f6fbe9250e0fa89a5fcf8c40b8b
rlechich <roy.lechich@yyale.edu>
2017-04-07 15:15:44 -0400

    adds Redis

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -38,10 +38,14 @@ class Annotation < ActiveRecord::Base
     iiif['motivation'] = motivation.split(",")
     #iiif['annnotatedBy'] = JSON.parse(annotated_by) if !annnotated_by.nil?
     iiif['on'] = on
-    p "anno_id = #{annotation_id} and on = #{on}"
+    #p "anno_id = #{annotation_id} and on = #{on}"
+    #begin
     if (on.start_with?("{"))
-      iiif['on'] = JSON.parse(on.gsub(/=>/,":"))
+        iiif['on'] = JSON.parse(on.gsub(/=>/,":"))
     end
+    #rescue
+    #  p "error in to_iiif: could not json.parse [on] for anno #{annotation_id}"
+    #end
     iiif#.to_json
   end
 
