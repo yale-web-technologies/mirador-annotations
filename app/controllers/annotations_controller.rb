89c96d1ab219065fd770be35de35b4766c7efd0c
rlechich <roy.lechich@yyale.edu>
2017-05-08 12:39:16 -0400

    ...

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -897,10 +897,9 @@ class AnnotationsController < ApplicationController
     # redisValue_Panel_01 = JSON.parse(redisValue_Panel_01)
     # @redis.set("http://manifests.ydc2.yale.edu/LOTB/canvas/panel_01",redisValue_Panel_01)
 
-    response = '{/"redisKey/":/"' + @canvasKey + '/" set/"}'
     respond_to do |format|
-      format.html { render html: response, status: :ok }
-      format.json { render json: response, status: :ok }
+      format.html { render html: 'RedisKey set', status: :ok }
+      format.json { render json: 'RedisKey set', status: :ok }
     end
   end
 
