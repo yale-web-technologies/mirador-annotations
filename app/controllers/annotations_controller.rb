5762151ec38655649505262f283d1841a82b924e
rlechich <roy.lechich@yyale.edu>
2017-07-20 13:31:26 -0400

    change delete method to just use the passed in anno_id as-is

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -378,19 +378,10 @@ class AnnotationsController < ApplicationController
   # DELETE /annotation/1.json
   def destroy
     p 'in annotation_controller:destroy'
-
-    #@ru = request.original_url   # will not work with rspec
-    #@ru1 = Rails.application.config.hostUrl  + "/annotations/#{params['id']}"
-    #@ru = Rails.application.config.hostUrl  + "annotations/#{params['id']}"
-    #@ru = params['id'] # for rspec
-    #@ru = request.protocol + request.host_with_port + "/annotations/#{params['id']}"
     request.format = "json"
     puts "\ndelete params: #{params.to_s}"
 
-    #@annotation = Annotation.where(annotation_id: @ru).first
     @annotation = Annotation.where("annotation_id like ? ", "%#{params['id']}").first
-
-    #@annotation = Annotation.where("annotation_id like ? ", "%#{on['full']}%").first
 
     if @annotation.nil?
       p 'did not find @annotation for destroy: ' + params['id']
